4e7fe74d6f2703b73736540ae30586016210ad11
MiguelHigueraDev <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-03-09 20:44:59 -0300

    fix: update schema

---
+++ src/prisma/migrations/20240309234310_5/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `lastRequest` on the `GuildUser` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `GuildUser` DROP COLUMN `lastRequest`;
