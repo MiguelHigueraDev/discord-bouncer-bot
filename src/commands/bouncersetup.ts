f54549b3d35ad6db63f4f40dd0230718d073e849
MiguelHigueraDev <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-03-09 13:59:58 -0300

    feat: finish bouncersetup

--- src/commands/bouncersetup.ts
+++ src/commands/bouncersetup.ts
@@ -1,5 +1,6 @@
 import { Command } from '@sapphire/framework'
-import { type ChatInputCommandInteraction, PermissionFlagsBits, type InteractionResponse } from 'discord.js'
+import { type ChatInputCommandInteraction, PermissionFlagsBits, type InteractionResponse, ChannelType } from 'discord.js'
+import guildHandler from '../lib/database/guildHandler'
 
 export class BouncerSetup extends Command {
   public constructor (context: Command.LoaderContext, options: Command.Options) {
@@ -24,6 +25,7 @@ export class BouncerSetup extends Command {
                 .setName('voice-channel')
                 .setDescription('The voice channel to set as private.')
                 .setRequired(true)
+                .addChannelTypes(ChannelType.GuildVoice)
             )
         )
         .addSubcommand((command) =>
@@ -33,6 +35,7 @@ export class BouncerSetup extends Command {
                 .setName('voice-channel')
                 .setDescription('The voice channel to set as the waiting room.')
                 .setRequired(true)
+                .addChannelTypes(ChannelType.GuildVoice)
             )
         )
         .addSubcommand((command) =>
@@ -42,9 +45,10 @@ export class BouncerSetup extends Command {
                 .setName('text-channel')
                 .setDescription('The text channel where all requests will be sent.')
                 .setRequired(true)
+                .addChannelTypes(ChannelType.GuildText)
             )
         ), {
-      idHints: []
+      idHints: ['1215894964060422206']
     })
   }
 
@@ -54,9 +58,46 @@ export class BouncerSetup extends Command {
       // Show help
     }
 
-    if (subcommand === 'set-waiting-vc' || subcommand === 'set-private-vc') {
-      const channel = interaction.options.getChannel('voice-channel', true)
-      if 
+    // Check this so typescript doesn't complain about the guild being possibly null
+    if (interaction.guild == null) {
+      return await interaction.reply({ content: 'Error fetching details from the server.', ephemeral: true })
     }
+
+    // Voice channels
+    if (subcommand === 'set-private-vc' || subcommand === 'set-waiting-vc') {
+      const channel = interaction.options.getChannel('voice-channel', true)
+
+      if (subcommand === 'set-private-vc') {
+        const updated = await guildHandler.updateGuildPrivateVc(interaction.guild.id, channel.id)
+
+        if (!updated) {
+          return await interaction.reply({ content: 'Error updating the private voice channel.', ephemeral: true })
+        } else {
+          return await interaction.reply({ content: 'Private voice channel updated.', ephemeral: true })
+        }
+      } else {
+        const updated = await guildHandler.updateGuildWaitingVc(interaction.guild.id, channel.id)
+
+        if (!updated) {
+          return await interaction.reply({ content: 'Error updating the waiting room voice channel.', ephemeral: true })
+        } else {
+          return await interaction.reply({ content: 'Waiting room voice channel updated.', ephemeral: true })
+        }
+      }
+    }
+
+    // Text channel
+    if (subcommand === 'set-text-channel') {
+      const channel = interaction.options.getChannel('text-channel', true)
+
+      const updated = await guildHandler.updateGuildTextChannel(interaction.guild.id, channel.id)
+      if (!updated) {
+        return await interaction.reply({ content: 'Error updating the text channel.', ephemeral: true })
+      } else {
+        return await interaction.reply({ content: 'Text channel updated.', ephemeral: true })
+      }
+    }
+
+    return await interaction.reply({ content: 'Invalid subcommand.', ephemeral: true })
   }
 }
