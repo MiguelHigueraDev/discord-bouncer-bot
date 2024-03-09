a9609b1c59545d0a4c12667dd0e96ee5df6cc134
MiguelHigueraDev <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-03-09 14:55:40 -0300

    feat: update guildHandler with functions to update channels

--- src/lib/database/guildHandler.ts
+++ src/lib/database/guildHandler.ts
@@ -40,8 +40,95 @@ const updateGuildStatus = async (guildId: string): Promise<boolean> => {
   }
 }
 
+/**
+ * Updates the private voice channel for a specific guild.
+ *
+ * @param {string} guildId - The ID of the guild
+ * @param {string} privateVcId - The ID of the private voice channel
+ * @return {Promise<boolean>} A boolean indicating whether the update was successful
+ */
+const updateGuildPrivateVc = async (guildId: string, privateVcId: string): Promise<boolean> => {
+  try {
+    const updatedGuild = await updateGuildStatus(guildId)
+    if (!updatedGuild) {
+      return false
+    }
+
+    await container.db.guild.update({
+      where: {
+        id: guildId
+      },
+      data: {
+        privateVcId
+      }
+    })
+    return true
+  } catch (error) {
+    console.error('An error has ocurred while running guildHandler:updateGuildPrivateVc', error)
+    return false
+  }
+}
+
+/**
+ * Updates the waiting voice channel for a guild and returns a boolean indicating success.
+ *
+ * @param {string} guildId - The ID of the guild
+ * @param {string} waitingVcId - The ID of the waiting voice channel
+ * @return {Promise<boolean>} A boolean indicating whether the update was successful
+ */
+const updateGuildWaitingVc = async (guildId: string, waitingVcId: string): Promise<boolean> => {
+  try {
+    const updatedGuild = await updateGuildStatus(guildId)
+    if (!updatedGuild) {
+      return false
+    }
+
+    await container.db.guild.update({
+      where: {
+        id: guildId
+      },
+      data: {
+        waitingVcId
+      }
+    })
+    return true
+  } catch (error) {
+    console.error('An error has ocurred while running guildHandler:updateGuildWaitingVc', error)
+    return false
+  }
+}
+
+/**
+ * Updates the text channel for a specific guild.
+ *
+ * @param {string} guildId - The ID of the guild
+ * @param {string} textChannelId - The ID of the text channel
+ * @return {Promise<boolean>} Whether the text channel was successfully updated
+ */
+const updateGuildTextChannel = async (guildId: string, textChannelId: string): Promise<boolean> => {
+  try {
+    const updatedGuild = await updateGuildStatus(guildId)
+    if (!updatedGuild) {
+      return false
+    }
+
+    await container.db.guild.update({
+      where: {
+        id: guildId
+      },
+      data: {
+        textChannelId
+      }
+    })
+    return true
+  } catch (error) {
+    console.error('An error has ocurred while running guildHandler:updateGuildTextChannel', error)
+    return false
+  }
+}
+
 const guildHandler = {
-  updateGuildStatus
+  updateGuildStatus, updateGuildPrivateVc, updateGuildWaitingVc, updateGuildTextChannel
 }
 
 export default guildHandler
