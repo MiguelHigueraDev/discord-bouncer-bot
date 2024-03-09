9d3550d62963fda4d601ea47f2f6d9507160ef1a
MiguelHigueraDev <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-03-09 17:19:55 -0300

    feat: add rememberedUsers and ignoredUsers store

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,13 @@ import { PrismaClient } from '@prisma/client'
 import { SapphireClient, container } from '@sapphire/framework'
 import { ActivityType, GatewayIntentBits } from 'discord.js'
 import { config } from 'dotenv'
+import { type Guild } from './lib/interfaces/Guild'
 config()
 
 const token = process.env.BOT_TOKEN
 
 const client = new SapphireClient({
-  intents: [GatewayIntentBits.Guilds],
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates],
   loadMessageCommandListeners: true,
   presence: {
     activities: [{
@@ -24,9 +25,13 @@ const prisma = new PrismaClient()
 declare module '@sapphire/pieces' {
   interface Container {
     db: PrismaClient
+    rememberedUsers: Guild[]
+    ignoredUsers: Guild[]
   }
 }
 
 container.db = prisma
+container.rememberedUsers = []
+container.ignoredUsers = []
 
 client.login(token).catch((error) => { console.log(`The bot has crashed.\n ${error}`) })
