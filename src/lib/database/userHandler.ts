2c789c620920f33c933830e7931d3a5550fbc175
MiguelHigueraDev <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-03-08 23:41:18 -0300

    feat: add userHandler

---
+++ src/lib/database/userHandler.ts
@@ -0,0 +1,115 @@
+import { container } from '@sapphire/framework'
+
+const updateUserStatus = async (userId: string): Promise<boolean> => {
+  try {
+    const foundUser = await container.db.user.findFirst({ where: { id: userId } })
+    const username = (await container.client.users.fetch(userId)).username
+
+    if (foundUser == null) {
+      await container.db.user.create({
+        data: {
+          id: userId,
+          username
+        }
+      })
+    } else {
+      await container.db.user.update({
+        where: {
+          id: userId
+        },
+        data: {
+          username
+        }
+      })
+    }
+    return true
+  } catch (error) {
+    console.error('An error has ocurred while running userHandler:updateUserStatus', error)
+    return false
+  }
+}
+
+const updateGuildUserStatus = async (userId: string, guildId: string): Promise<boolean> => {
+  try {
+    const foundGuildUser = await container.db.guildUser.findFirst({ where: { userId, guildId } })
+    if (foundGuildUser == null) {
+      await container.db.guildUser.create({
+        data: {
+          userId,
+          guildId
+        }
+      })
+    }
+    return true
+  } catch (error) {
+    console.error('An error has ocurred while running userHandler:updateGuildUserStatus', error)
+    return false
+  }
+}
+
+/**
+ * Toggles the blocklist status of a user within a specific guild.
+ * If the user exists, updates the blocklist status. If not, creates a new record.
+ *
+ * @param {string} userId - The unique identifier of the user.
+ * @param {string} guildId - The unique identifier of the guild.
+ * @param {boolean} [blocklisted=true] - The desired blocklist status of the user. Default is true.
+ * @returns {Promise<boolean>} A promise that resolves to true if the operation was successful, false otherwise.
+ * @throws {Error} Throws an error if an error occurs during the operation.
+ *
+ */
+const toggleBlocklistStatus = async (userId: string, guildId: string, blocklisted: boolean = true): Promise<boolean> => {
+  try {
+    const foundGuildUser = await container.db.guildUser.findFirst({ where: { userId, guildId } })
+    if (foundGuildUser == null) {
+      // GuildUser doesn't exist, create it.
+      const created = await updateGuildUserStatus(userId, guildId)
+      if (!created) return false
+    }
+
+    await container.db.guildUser.update({
+      where: {
+        userId,
+        guildId
+      },
+      data: {
+        blocklisted
+      }
+    })
+    return true
+  } catch (error) {
+    console.error('An error has ocurred while running userHandler:addUserToBlocklist', error)
+    return false
+  }
+}
+
+const toggleAllowlistStatus = async (userId: string, guildId: string, allowlisted: boolean = true): Promise<boolean> => {
+  try {
+    const foundGuildUser = await container.db.guildUser.findFirst({ where: { userId, guildId } })
+    if (foundGuildUser == null) {
+      // GuildUser doesn't exist, create it.
+      const created = await updateGuildUserStatus(userId, guildId)
+      if (!created) return false
+    }
+
+    await container.db.guildUser.update({
+      where: {
+        userId,
+        guildId
+      },
+      data: {
+        allowlisted
+      }
+    })
+    return true
+  } catch (error) {
+    console.error('An error has ocurred while running userHandler:addUserToAllowlist', error)
+    return false
+  }
+}
+
+const userHandler = {
+  updateUserStatus, updateGuildUserStatus, toggleBlocklistStatus, toggleAllowlistStatus
+}
+
+export default userHandler
