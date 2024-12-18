57b5179f0e49534e0a6fd0e1bfa2ae44d4501e4d
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-12-18 11:19:39 -0300

    sd

--- src/listeners/sessionEndVoiceUpdateListener.ts
+++ src/listeners/sessionEndVoiceUpdateListener.ts
@@ -2,48 +2,54 @@
  * This one destroys sessions when all users leave the private VC
  */
 
-import { Listener } from '@sapphire/framework'
-import { EmbedBuilder, type VoiceState } from 'discord.js'
-import guildHandler from '../lib/database/guildHandler'
-import sessionManager from '../lib/helpers/sessionManager'
+import { Listener } from "@sapphire/framework";
+import { EmbedBuilder, type VoiceState } from "discord.js";
+import guildHandler from "../lib/database/guildHandler";
+import sessionManager from "../lib/helpers/sessionManager";
 
 export class SessionEndVoiceStateUpdateListener extends Listener {
-  public constructor (context: Listener.LoaderContext, options: Listener.Options) {
+  public constructor(
+    context: Listener.LoaderContext,
+    options: Listener.Options
+  ) {
     super(context, {
       ...options,
-      event: 'voiceStateUpdate'
-    })
+      event: "voiceStateUpdate",
+    });
   }
 
-  public async run (oldState: VoiceState, newState: VoiceState) {
-    // Check if session exists in the first place
-    if (!sessionManager.checkIfSessionExists(oldState.guild.id)) return
+  public async run(oldState: VoiceState, newState: VoiceState) {
+    if (!sessionManager.checkIfSessionExists(oldState.guild.id)) return;
 
-    const leftChannel = oldState.channelId
-    const guildPrivateVcId = await guildHandler.getGuildPrivateVcId(newState.guild.id)
-    if (leftChannel === guildPrivateVcId) {
-      if (oldState.channel!.members.size === 0) {
-        // Check channel is valid
-        const guildTextChannelId = await guildHandler.getGuildTextChannelId(oldState.guild.id)
-        if (guildTextChannelId == null) return
-        const guildTextChannel = await oldState.guild.channels.fetch(guildTextChannelId)
-        if (guildTextChannel == null) return
+    const leftChannel = oldState.channelId;
+    const guildPrivateVcId = await guildHandler.getGuildPrivateVcId(
+      newState.guild.id
+    );
+    if (
+      leftChannel === guildPrivateVcId &&
+      oldState.channel?.members.size === 0
+    ) {
+      const guildTextChannelId = await guildHandler.getGuildTextChannelId(
+        oldState.guild.id
+      );
+      const guildTextChannel = await oldState.guild.channels.fetch(
+        guildTextChannelId
+      );
 
-        // Destroy session
-        const embed = this.makeEmbed()
-        if (guildTextChannel.isTextBased()) {
-          sessionManager.destroySession(oldState.guild.id)
-          await guildTextChannel.send({ embeds: [embed] })
-        }
+      if (guildTextChannel?.isTextBased()) {
+        sessionManager.destroySession(oldState.guild.id);
+        await guildTextChannel.send({ embeds: [this.makeEmbed()] });
       }
     }
   }
 
   private readonly makeEmbed = (): EmbedBuilder => {
     const embed = new EmbedBuilder()
-      .setColor('Red')
-      .setTitle('Session ended')
-      .setDescription('All people have left the private voice channel. The session has ended.')
-    return embed
-  }
+      .setColor("Red")
+      .setTitle("Session ended")
+      .setDescription(
+        "All people have left the private voice channel. The session has ended."
+      );
+    return embed;
+  };
 }
